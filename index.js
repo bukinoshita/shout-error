412677064ee9c2ad189d4db18f82aa1ac88d8934
Bu Kinoshita <bukinoshita@gmail.com>
2019-12-06 15:11:38 -0300

    fix: lint

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-'use strict'
-
 const chalk = require('chalk')
 
 module.exports = msg => {
   if (msg.message) {
     msg = msg.message
   }
+
   console.error(`${chalk.red('> Error!')} ${msg}`)
 }
